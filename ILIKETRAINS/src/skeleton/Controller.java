a0db1c3f71f3a343f9218f37e92a653c4e404f05
barabali <balintbarath@gmail.com>
2017-04-17 22:38:09 +0200

    controller fix

--- ILIKETRAINS/src/skeleton/Controller.java
+++ ILIKETRAINS/src/skeleton/Controller.java
@@ -158,7 +158,6 @@ public class Controller {
 			File file = null;
 			String filename="";
 			filename=Game.generateFilename("test_input_"+line+".txt");
-			System.out.println(filename);
 			file = new File(filename);
 
 			//Beolvassa a tesztparancsokat
